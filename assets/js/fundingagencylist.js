852b361a91a70471827a1c806084594404351821
mbgrava10 <gravitas.maser@gmail.com>
2016-10-18 23:38:05 +0800

    Add files via upload

---
+++ assets/js/fundingagencylist.js
@@ -0,0 +1,57 @@
+amplify.store("fundingagencyList", [
+  {
+    "id": "fa1",   
+    "name":"Philippine Charity Sweepstakes Office",
+	"website":"http://www.pcso.gov.ph/",
+	"image":"images/pcsosmall.png",
+	"mission":"To generate funds for health programs and charities of national character through charity sweepstakes races, lotteries and other similar activities",
+	"about":"Initially serving its officials and employees, referrals and walk-ins, the PCSO Charity Clinic’s services have now expanded to include the depressed barangays in Metro Manila to as far as Sorsogon in the South and Isabela in the North. A total of twenty-six (26) outreach missions have been conducted during the first year of the program. Currently, a total of 117,001 medical patients and 28,292 dental patients have benefited from these undertakings (figures as of 2001). The Out-Patient Clinic Department of the PCSO renders services from Monday to Friday to thousands of indigent patients at the PCSO Dental and Clinic Department in Gate 6, City Hall, Kalayaan St., Quezon City.. The clinic department offers the following services: free consultations, minor surgeries, ambulance conduction of patients and emergency care as well as referrals. It also undertakes community outreach projects in Maternal and Child Health and Primary Health Care. A total of 36,911 medical and 18,965 dental patients have been the recipient of this endeavor (as of 2001).",
+	"contactperson":"Mr. Rubin Z. Magno",
+	"contactdesignation":"OIC-Department Manager",
+	"contactdepartment":"Charity Assistance Department",
+	"contactdetails":"426-3735, 921-7608",
+	"email":" rmagno@pcso.gov.ph",
+	"patients":[
+	{
+	"pxid":"px2",
+	"name":"Yoon Eun-hye",
+	"approvaldate":"30-Sept-2016",
+	"status":"",
+	},
+	{
+	"pxid":"px4",
+	"name":"Park Shin Hye",
+	"approvaldate":"3-Sept-2016",
+	"status":"",
+	},
+	],
+  },
+   {
+    "id": "fa2",   
+    "name":"THE MAHARLIKA CHARITY FOUNDATION, INC.",
+	"website":"http://www.maharlikafoundation.org/index.html",
+	"image":"images/maharlika.jpg",
+	"mission":"Forging partnerships between government, business and non-governmental sectors, Maharlika provides free surgical services to the poor and the needy. Its volunteer doctors, dentists and nurses have participated in several surgical outreach missions in the remotest areas of Mindanao.",
+	"about":"SINCE THE MAHARLIKA CHARITY FOUNDATION was founded in May 1973, thousands of poor, disabled patients, burn victims, those suffering from eye defects, congenital anomalies, and other diseases were treated free by some of Davao's best medical and dental specialists. The Foundation, supported wholly by donations from individual benefactors, companies, fund-raising drives, government charity agencies, foreign contributions, and even token monies from the ordinary people, is now a modern medical facility; and manned by well trained volunteer medical team. It has its own building, the Maharlika Center, at J.P. Cabaguio Avenue, Davao City.",
+	"contactperson":"Rodrigo Toria",
+	"contactdesignation":"",
+	"contactdepartment":"",
+	"contactdetails":"(82)300.5979 or (82)300.6234",
+	"email":"share@maharlikafoundation.org",
+	"patients":[
+	{
+	"pxid":"px3",
+	"name":"Yoon Eun-hye",
+	"approvaldate":"30-Sept-2016",
+	"status":"",
+	},
+	{
+	"pxid":"px6",
+	"name":"Park Shin Hye",
+	"approvaldate":"3-Sept-2016",
+	"status":"",
+	},
+	],
+  },
+  
+]);
---
+++ assets/js/pharmalist.js
@@ -0,0 +1,38 @@
+amplify.store("pharmaList", [
+{
+"pharmid":"ABC",
+"pharmaname":"ABC Co",
+"image":"images/abc.jpg",
+"about":"ABC Co is dedicated to discovering and developing innovative medicines and making them available to people around the world. <br/> Our business is fundamentally about saving, improving and enhancing lives, and we believe that by being successful as a business, we provide the greatest benefit to society.",
+"reps":[
+{
+	"repname":"Han Ga In",
+	"repnumber":"+6329784562",
+	"repemail":"gain.han@abcco.com",
+},
+{
+	"repname":"Dona Reyes",
+	"repnumber":"+639178528522",
+	"repemail":"reyes.dona@abcco.com",
+},
+],
+},
+{
+"pharmid":"DEF",
+"pharmaname":"DEF Corp",
+"image":"images/def.jpg",
+"about":"DEF Corp is engaged in the manufacturing and marketing of Formulations (Finished Dosage Forms) and Active Pharmaceutical Ingredients (APIs) in India. DEF Corp has a strong international presence in the Regulated and Emerging markets. The Company is striving to place itself on a strong promising path by expanding its business strategically, strengthening its manufacturing facilities and enhancing capabilities across the organization.",
+"reps":[
+{
+	"repname":"Kwon Sang Woo",
+	"repnumber":"+63297894561",
+	"repemail":"kswoo123@defcorp.com",
+},
+{
+	"repname":"Genevieve dela Torre",
+	"repnumber":"+639277412581",
+	"repemail":"gdelator234@defcorp.com",
+},
+],
+},
+]);
